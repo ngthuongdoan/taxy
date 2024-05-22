145a5ccd2a3923fc08f8d8df3225b5f671e84efa
Ky Huynh <tankyhuynh99@gmail.com>
2024-05-22 14:15:41 +0700

    fix: Fix the build issue

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.testjavafx {
+module com.example.taxy {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires org.apache.poi.ooxml;
     requires java.datatransfer;
+    requires org.apache.poi.poi;
 
     opens com.example.taxy to javafx.fxml;
     exports com.example.taxy;
